796c8f507bdf11d361da5a2bfa03abeb70864265
Mungstrocity <jarett@mungstrocity.com>
2025-03-05 15:07:30 -0600

    made some changes

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -124,7 +124,7 @@ module controller (input  logic [6:0] op,
 		   output logic [2:0] ImmSrc,
 		   output logic [2:0] ImmSrc,           //3-bit - KM
 		   output logic [2:0] ALUControl);
-   
+   //some change//
    logic [1:0] 			      ALUOp;
    logic 			      Branch;
    
