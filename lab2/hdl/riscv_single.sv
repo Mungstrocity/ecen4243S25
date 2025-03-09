6f63a4e2868a02a734db11e9cbc838b7dfc2951f
Mungstrocity <jarett@mungstrocity.com>
2025-03-08 22:22:27 -0600

    adding lui test

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -238,7 +238,7 @@ module datapath (input  logic        clk, reset,
 		 output logic 	     Zero,
 		 output logic [31:0] PC,
 		 input  logic [31:0] Instr,
-		 output logic [31:0] ALUResult, WriteData,
+		 output logic [31:32] ALUResult, WriteData,
 		 input  logic [31:0] ReadData);
    
    logic [31:0] 		     PCNext, PCPlus4, PCTarget;
